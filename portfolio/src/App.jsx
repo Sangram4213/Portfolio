a9cfcabe07d3ebb35c77bcb8338f614faaad9237
Sangram4213 <92009582+Sangram4213@users.noreply.github.com>
2024-11-30 18:20:43 +0530

    add project section

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,11 +1,13 @@
 import './styles/app.scss';
 import Hero from './sections/Hero/Hero';
+import Project from './sections/Project/Project';
 
 function App() {
 
   return (
     <>
       <Hero/>
+      <Project/>
     </>
   );
 }
---
+++ portfolio/src/sections/Project/Project.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import eCommerceImg from '../../assets/ecommerce.png';
+import chatAppImg from '../../assets/ChatApp.jpeg';
+import ProjectCard from "./ProjectCard";
+
+const Project = () => {
+  return (
+    <section id="projects" className="project-container">
+      <h1 className="sectionTitle">Project</h1>
+      <div className='projectContainer'>
+        <ProjectCard src={eCommerceImg} link="https://github.com/Sangram4213/MERN-ECommerce-FrontEnd" h3="eCommerece" p="Shopping App"/>
+        <ProjectCard src={chatAppImg} link="https://github.com/Sangram4213/ChatApp" h3="ChatApp" p="Chatting App"/>
+      </div>
+    </section>
+  );
+};
+
+export default Project;
